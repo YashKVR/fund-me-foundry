b4e1085e0d7450abc0d8a6cbd37fb355c901822d
YashKVR <yashincrypto5@gmail.com>
2024-06-25 09:25:25 +0530

    added basic test

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,21 @@
+// SDPX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test{
+    FundMe fundme;
+    function setUp() external{
+        fundme = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() view public{
+        assertEq(fundme.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() view public{
+        assertEq(fundme.i_owner(), address(this));
+    }
+}
